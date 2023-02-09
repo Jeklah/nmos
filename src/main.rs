9b56e4f51905b227d400c6b67f99ec17c21c2e4b
Jeklah <arthurbowers@gmail.com>
2023-02-09 21:48:23 +0000

    Changing the implementation

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,38 @@
-// an implementation of IS-04 abd IS-05 for NMOS in rust
+// an implementation of IS-04 for NMOS in rust
+//
 
-use std::env;
+use hyper::rt::{Future, Stream};
+use hyper::Client;
 use std::fs::File;
 use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
+use std::{env, io};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let ip = &args[1];
-    let port = &args[2];
-    let address = format!("{}:{}", ip, port);
-    println!("Listening on {}", address);
-    let listener = TcpListener::bind(address).unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        thread::spawn(|| {
-            handle_connection(stream);
-        });
-    }
-}
+    // register the node
+    let mut file = File::open("node.json").expect("Unable to open file");
+    let mut node = String::new();
+    file.read_to_string(&mut node).expect("Unable to read file");
+    let client = Client::new();
+    let uri = "http://localhost:8080/x-nmos/node/v1.0/nodes"
+        .parse()
+        .unwrap();
+    let work = client
+        .post(uri)
+        .body(node)
+        .and_then(|res| {
+            println!("Response: {}", res.status());
+            println!(
+                "Headers: {:#?}
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let get = b"GET / HTTP/1.1\r";
-    let sleep = b"GET /sleep HTTP/1.1\r";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\r", "hello.html")
-    } else if buffer.starts_with(sleep) {
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\r", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\r", "404.html")
-    };
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    let response = format!("{}{}", status_line, contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+",
+                res.headers()
+            );
+            res.into_body()
+                .for_each(|&chunk| io::stdout().write_all(&chunk).map_err(From::from))
+        })
+        .map_err(|err| {
+            println!("Error: {}", err);
+        });
+
+    hyper::rt::run(work);
 }
