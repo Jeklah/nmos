763357225cbe05634875fc9246a71c14c0d4dcd3
ArthurB <arthur.bowers@phabrix.com>
2023-03-21 13:51:37 +0000

    Changing implementation of admin_ui

--- src/admin_ui.rs
+++ src/admin_ui.rs
@@ -1,61 +1,83 @@
+use crate::nmos::{api_utils::*, filesystem_route::*, slog::*, *};
+use std::convert::TryFrom;
 use std::sync::Arc;
-use hyper::{Body, Method, Request, Response, StatusCode};
-use serde::Serialize;
-use futures::{FutureExt, TryFutureExt};
-use crate::nmos::{make_sub_routes_body, make_route_pattern};
-use crate::nmos::slog::Logger;
-use crate::nmos::api_utils::{set_reply, make_filesystem_route, make_relative_path_content_type_handler};
+use tokio::sync::Mutex;
+use warp::http::{header::HeaderValue, header::LOCATION, Response, StatusCode};
+use warp::{Filter, Reply};
 
-pub fn make_api_sub_route(sub_route: &str, sub_router: hyper::service::make_service_fn, logger: Arc<Logger>) -> hyper::service::make_service_fn {
-    let api = make_route_pattern("api", sub_route);
+pub fn make_api_sub_route(
+    sub_route: &str,
+    sub_router: warp::filters::BoxedFilter<(impl Reply,)>,
+    gate: Arc<Mutex<Box<dyn Gate>>>,
+) -> warp::filters::BoxedFilter<(impl Reply,)> {
+    let api = format!("/api/{}", sub_route);
+    let sub_routes = format!("{}/*", sub_route);
 
-    let make_svc = move |req: Request<Body>| {
-        let logger = logger.clone();
-
-        let res = match (req.method(), req.uri().path()) {
-            (&Method::GET, path) if path == "/" => {
-                let body = make_sub_routes_body(&[sub_route], req);
-                set_reply(Response::builder(), StatusCode::OK, Some(body))
-            },
-            (&Method::GET, path) if path == api.pattern => {
-                let location = format!("{}{}", path, '/');
-                Response::builder()
-                    .status(StatusCode::TEMPORARY_REDIRECT)
-                    .header("Location", location)
-                    .body(Body::empty())
-                    .unwrap()
-            },
-            (&Method::GET, path) if path.starts_with(&(api.pattern + "/")) => {
-                let sub_path = path.trim_start_matches(&(api.pattern + "/"));
-                sub_router(req, sub_path.to_owned())
-            },
-            _ => {
-                let body = format!("Method {} not allowed", req.method());
-                set_reply(Response::builder(), StatusCode::METHOD_NOT_ALLOWED, Some(body))
-            }
-        };
+    let router = warp::any().and_then(move || {
+        let gate = gate.clone();
+        let api_clone = api.clone();
 
         async move {
-            logger.log_response(res.status().as_u16(), &req, &res);
-            Ok::<_, hyper::Error>(res)
-        }.boxed().unit_error().compat()
-    };
+            Ok(Response::builder()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+                .header("Access-Control-Allow-Headers", "Content-Type")
+                .header("Access-Control-Max-Age", "86400")
+                .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                .header("Pragma", "no-cache")
+                .header("Expires", "0")
+                .header("X-Content-Type-Options", "nosniff")
+                .header("X-Frame-Options", "SAMEORIGIN")
+                .header("X-XSS-Protection", "1; mode=block")
+                .header("Content-Security-Policy", "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self'")
+                .header("Referrer-Policy", "strict-origin-when-cross-origin")
+                .status(StatusCode::OK)
+                .body(make_sub_routes_body(
+                    &[sub_routes.as_str().to_owned()],
+                    &api_clone,
+                    &HeaderValue::from_str("http://localhost:3209/").unwrap(),
+                    None,
+                    gate.lock().await.deref_mut(),
+                ))?)
+        }
+    });
 
-    make_svc
+    warp::path::end()
+        .and(warp::get())
+        .and(router)
+        .boxed()
+        .or(warp::path(api.as_str())
+            .and(warp::get())
+            .map(|| warp::redirect::temporary(Uri::try_from(sub_routes).unwrap())))
+        .or(warp::path(api)
+            .and(warp::path::tail())
+            .and(sub_router)
+            .boxed())
+        .boxed()
 }
 
-pub fn make_admin_ui(filesystem_root: &str, logger: Arc<Logger>) -> hyper::service::make_service_fn {
-    let valid_extensions = vec![
+pub fn make_admin_ui(
+    filesystem_root: &str,
+    gate: Arc<Mutex<Box<dyn Gate>>>,
+) -> warp::filters::BoxedFilter<(impl Reply,)> {
+    // To serve the admin UI, only a few HTML, JavaScript and CSS files are necessary
+    let valid_extensions: HashMap<&str, &str> = vec![
         ("ico", "image/x-icon"),
         ("html", "text/html"),
         ("js", "application/javascript"),
         ("map", "application/json"),
         ("json", "application/json"),
         ("css", "text/css"),
-        ("png", "image/png")
-    ];
+        ("png", "image/png"),
+    ]
+    .into_iter()
+    .collect();
 
-    let content_type_handler = make_relative_path_content_type_handler(valid_extensions);
-    let sub_router = make_filesystem_route(filesystem_root, content_type_handler, logger.clone());
-    make_api_sub_route("admin", sub_router, logger)
+    let handler = make_relative_path_content_type_handler(valid_extensions);
+
+    make_api_sub_route(
+        "admin",
+        make_filesystem_route(filesystem_root, handler, gate),
+        gate,
+    )
 }
